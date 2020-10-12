3a24edd06b592ab2d0292a34c11f353e1b00109d
Florent Frossard <florentwm@gmail.com>
2020-10-12 23:12:29 +0200

    Initial Commit

---
+++ Fruta/FrutaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrutaApp.swift
+//  Fruta
+//
+//  Created by Florent on 12/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct FrutaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
